a99d8520465d700df1867d35dd018d6244a56416
arafkubraa <kbrtln06@gmail.com>
2025-02-17 01:11:07 +0300

    feat(product):fetch products from JSON file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
 import { CategoryComponent } from "./category/category.component";
 import { ProductComponent } from "./product/product.component";
+import { HttpClientModule } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavComponent, CategoryComponent, ProductComponent],
+  imports: [RouterOutlet, NavComponent, CategoryComponent, ProductComponent,HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -3,6 +3,10 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Product } from './product';
 import { ProductFilterPipe } from "./product-filter.pipe";
+import { ProductService } from '../services/product.service';
+
+
+
 
 
 @Component({
@@ -13,28 +17,25 @@ import { ProductFilterPipe } from "./product-filter.pipe";
     ProductFilterPipe
 ],
   templateUrl: './product.component.html',
-  styleUrl: './product.component.css'
+  styleUrl: './product.component.css',
+  providers:[ProductService]
 })
 export class ProductComponent implements OnInit {
 
-  constructor() {  }
+  constructor(private productService:ProductService) {  }
 
   title= "Quotes"
   filterText=""
+  products!: Product[];
 
-  products: Product[]=[
-    {id:1,name:"Arcane",categoryId:1,quote:"“Bazen ileri adım atmak, birkaç şeyi geride bırakmak demektir.“",imageUrl:"https://i.pinimg.com/736x/48/59/59/4859596fddfd80dffbf8f512bb2f30aa.jpg",siteUrl:"https://1000kitap.com/arcane--5904371"},
-    {id:1,name:"11.22.63",categoryId:1,quote:"“Seni seviyorum. Şimdi ve gelecekte.“",imageUrl:"https://i.pinimg.com/736x/3a/f1/ef/3af1ef78363bd374d79dbd166ba6c138.jpg",siteUrl:"https://1000kitap.com/kitap/22-11-63--3019/alintilar"},
-    {id:3,name:"Pride and Prejudice",categoryId:2,quote:"“İnsan isteyince mesafenin önemi yoktur.”",imageUrl:"https://i.pinimg.com/736x/3f/ca/fd/3fcafda0612cf921df811a10b76812cf.jpg",siteUrl:"https://1000kitap.com/kitap/gurur-ve-onyargi--1654/alintilar"},
-    {id:4,name:"✨",categoryId:1,quote:"“Gözlerini parlatan şey için savaşmamak ne büyük korkaklık.“",imageUrl:"https://i.pinimg.com/736x/db/06/47/db0647dffe12bfc04d3a266edd92ff8c.jpg",siteUrl:"https://1000kitap.com/alintilar"},
-    {id:1,name:"Gölge & Kemik",categoryId:2,quote:"“Ben seni her dakika özledim. En kötüsü de neydi biliyor musun? Sana bir şey söylemek için ya da sırf sesini duyabilmek için etrafta seni aramaya alışmışken, günün birinde artık yanımda olmadığını anlamamdı.“",imageUrl:"https://i.pinimg.com/736x/95/b8/ed/95b8ed24f51613bdc9a89c153f03554f.jpg",siteUrl:"https://1000kitap.com/kitap/golge-ve-kemik--4151/alintilar"},
-    {id:4,name:"🌑",categoryId:2,quote:"“Güneş gözlerini acıttığı için gölgeleri savunuyorsun.“",imageUrl:"https://i.pinimg.com/736x/07/71/bd/0771bd9f6eec9c9d5c43c1528abd66b2.jpg",siteUrl:"https://1000kitap.com/alintilar"},
-    {id:4,name:"🌠",categoryId:1,quote:"“Sen bir yıldızsın, zifirin bile örtemediği.“",imageUrl:"https://i.pinimg.com/736x/8b/b3/c2/8bb3c200f7aec7a909aaeb67588ec268.jpg",siteUrl:"https://1000kitap.com/alintilar"},
-    {id:3,name:"Haruki Murakami",categoryId:2,quote:"“Ama öylece oturup sonsuza kadar yaralarımıza bakamayız.“",imageUrl:"https://i.pinimg.com/736x/af/2c/f1/af2cf190ec17b6f1686fca2d06dfbe22.jpg",siteUrl:"https://1000kitap.com/yazar/haruki-murakami/alintilar"}
-   ];
+
 
   ngOnInit(): void {
-  console.log("ProductComponent initialized");
+
+    this.productService.getQuotes().subscribe(data=>{
+     this.products=data
+    })
+
   }
 
   goSite(url: string) {
